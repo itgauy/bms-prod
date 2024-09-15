2ab96cfe1a3ce1d92b3b0b812cbfc41cb96d0c48
itgauy <torreslian@duck.com>
2024-09-15 20:52:19 +0800

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::view('/', 'posts.index')->name('home');
+
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('register');
