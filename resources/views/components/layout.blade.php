45546f26a4f7da01ca427d47dc53b2aeccfd0cc9
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-09-21 15:55:18 +0800

    Added user dropdown in main layout for logged in user with dummy content for testing

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -14,15 +14,14 @@
 <body class="bg-neutral-50">
   <!-- Layout Header -->
   <header class="fixed top-0 w-full bg-white shadow-lg shadow-neutral-300/10 z-40">
-    <nav class="md:p-6 px-6 md:py-6 py-4 flex items-center justify-between w-full md:px-16">
-
+    <nav class="md:p-6 px-4 md:py-6 py-4 flex items-center justify-between w-full md:px-16">
       <div class="flex items-center gap-5">
         <!-- FOR LOGGED IN USERS: Sidebar Toggle Button  -->
-        <!-- <button type="button" class="flex shrink-0 btn btn-secondary px-2 py-2" aria-haspopup="dialog" aria-expanded="false" aria-controls="main-sidebar" data-hs-overlay="#main-sidebar">
+        <button type="button" class="flex shrink-0 btn btn-secondary px-2 py-2" aria-haspopup="dialog" aria-expanded="false" aria-controls="main-sidebar" data-hs-overlay="#main-sidebar">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="shrink-0 size-5">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
           </svg>
-        </button> -->
+        </button>
         <!-- Logo/Home -->
         <a href="{{ route('home') }}" class="flex items-center gap-4">
           <span class="block rounded-full size-7 bg-neutral-300 ring-2 ring-neutral-400"></span>
@@ -31,23 +30,33 @@
       </div>
 
       <!-- FOR LOGGED OUT USERS: Sidebar Toggle Button for Mobile -->
-      <button type="button" class="lg:hidden flex shrink-0 btn btn-secondary px-2 py-2" aria-haspopup="dialog" aria-expanded="false" aria-controls="main-sidebar" data-hs-overlay="#main-sidebar">
+      <!-- <button type="button" class="lg:hidden flex shrink-0 btn btn-secondary px-2 py-2" aria-haspopup="dialog" aria-expanded="false" aria-controls="main-sidebar" data-hs-overlay="#main-sidebar">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="shrink-0 size-5">
           <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
         </svg>
-      </button>
+      </button> -->
+      <!-- FOR LOGGED IN USERS: User Dropdown Button -->
+      <div class="hs-dropdown relative inline-flex">
+        <button id="user-dropdown" class="btn btn-secondary flex items-center" aria-haspopup="menu" aria-expanded="false" aria-label="Dropdown">
+          Juan Dela Cruz
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="shrink-0 size-4">
+            <path fill-rule="evenodd" d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z" clip-rule="evenodd" />
+          </svg>
+        </button>
+        <x-modals.user-dropdown />
+      </div>
 
       <!-- FOR LOGGED OUT USERS: Navigation Links -->
-      <ul class="items-center gap-10 lg:flex hidden">
+      <!-- <ul class="items-center gap-10 lg:flex hidden">
         <li><a href="{{ route('home') }}#announcements">Announcements</a></li>
         <li><a href="#">Services</a></li>
         <li><a href="#">Officials</a></li>
         <li><a href="#">About us</a></li>
         <li><a href="#">Contact us</a></li>
-      </ul>
+      </ul> -->
 
       <!-- FOR LOGGED OUT USERS: Login and Register Buttons -->
-      <div class="items-center gap-4 lg:flex hidden">
+      <!-- <div class="items-center gap-4 lg:flex hidden">
         <a href="{{ route('login') }}" class="btn btn-secondary">Login</a>
         <button type="button" class="btn btn-primary" aria-haspopup="dialog" aria-expanded="false" aria-controls="signup-modal"
           data-hs-overlay="#signup-modal">Register
@@ -55,7 +64,7 @@
             <path fill-rule="evenodd" d="M8.25 3.75H19.5a.75.75 0 0 1 .75.75v11.25a.75.75 0 0 1-1.5 0V6.31L5.03 20.03a.75.75 0 0 1-1.06-1.06L17.69 5.25H8.25a.75.75 0 0 1 0-1.5Z" clip-rule="evenodd" />
           </svg>
         </button>
-      </div>
+      </div> -->
     </nav>
   </header>
 
---
+++ resources/views/components/modals/user-dropdown.blade.php
@@ -0,0 +1,36 @@
+<div class="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md rounded-lg p-1 space-y-0.5 mt-2 dark:bg-gray-800 dark:border dark:border-gray-700/50" role="menu" aria-orientation="vertical" aria-labelledby="user-dropdown">
+  <div class="py-5 flex flex-col justify-center items-center gap-3">
+    <span class="inline-flex items-center justify-center size-12 text-sm font-semibold rounded-full leading-none border border-gray-300 bg-indigo-600 text-white focus:ring-2 focus:ring-indigo-500 transition duration-300 ease-in-out">
+      FM
+    </span>
+    <div class="space-y-1 text-center">
+      <h3 class="dark:text-gray-400 text-gray-600 text-md">Frederick Moreno</h3>
+      <p class="dark:text-gray-400 text-gray-600 text-xs">frederick.moreno@email.com</p>
+    </div>
+  </div>
+  <div class="mt-2 py-2 first:pt-0 last:pb-0">
+    <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700/40 dark:hover:text-gray-300 dark:focus:bg-gray-700 transition duration-300" href="#">
+      <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
+        <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
+      </svg>
+      Newsletter
+    </a>
+    <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700/40 dark:hover:text-gray-300 dark:focus:bg-gray-700 transition duration-300" href="#">
+      <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <circle cx="8" cy="21" r="1" />
+        <circle cx="19" cy="21" r="1" />
+        <path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12" />
+      </svg>
+      Purchases
+    </a>
+    <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700/40 dark:hover:text-gray-300 dark:focus:bg-gray-700 transition duration-300" href="#">
+      <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <path d="M4 14.899A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.242" />
+        <path d="M12 12v9" />
+        <path d="m8 17 4 4 4-4" />
+      </svg>
+      Downloads
+    </a>
+  </div>
+</div>
