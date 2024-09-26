65f4069323083a9e63740cfea2d8e901775728c9
itgauy <torreslian@duck.com>
2024-09-26 23:44:40 +0800

    User Registration Working

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,10 @@
   <body class="md:h-screen h-auto">
     <main class="flex justify-center items-center h-full md:p-6 p-4 md:pt-36 md:py-24 py-24">
       <!-- Main Container -->
-      <form class="md:p-12 lg:p-16 p-6 shadow-lg shadow-neutral-100/60 bg-white rounded-3xl space-y-6 w-full max-w-3xl">
+      <form action="{{ route('register') }}" method="post"
+        class="md:p-12 lg:p-8 p-6 shadow-lg shadow-neutral-100/60 bg-white rounded-3xl space-y-6 w-full max-w-3xl">
+        @csrf
+
         <h2>Registration Form</h2>
         <div class="flex items-start gap-4">
           <span class="flex items-center justify-center w-8 h-8 border border-neutral-300 rounded-full shrink-0">
@@ -17,44 +20,67 @@
         </div>
         </div>
         <div class="grid md:grid-cols-2 grid-cols-1 gap-x-5 md:gap-y-6 gap-y-5 pt-4">
-          <!-- User -->
+          <!-- User Type -->
           <div class="md:col-span-2 col-span-1 md:space-y-2.5 space-y-1.5">
-            <label for="username" class="input-label">Are you registering as?</label>
+            <label for="home-owner" class="input-label">Are you registering as?</label>
             <div class="grid grid-cols-2 gap-x-5">
-              <label for="home-owner" class="grouped-radio">
+              <label for="home-owner" class="grouped-radio @error('user_type') !border-red-500 !bg-red-500/5 @enderror">
                 <div class="flex items-center gap-3">
-                  <input id="home-owner" type="radio" name="language" class="radio-input" />
+                  <input id="home-owner" type="radio" name="user_type" value="home-owner" class="radio-input" />
                   <span class="font-medium">Home Owner</span>
                 </div>
               </label>
-              <label for="renter-tenant" class="grouped-radio">
+              <label for="renter-tenant"
+                class="grouped-radio @error('user_type') !border-red-500 !bg-red-500/5 @enderror">
                 <div class="flex items-center gap-3">
-                  <input id="renter-tenant" type="radio" name="language" class="radio-input" />
+                  <input id="renter-tenant" type="radio" name="user_type" value="renter-tenant" class="radio-input" />
                   <span class="font-medium">Renter/Tenant</span>
                 </div>
               </label>
+              @error('user_type')
+                <p style="color:red;font-size:0.8rem;margin-top:-0.1rem;">{{ 'Choose at least one category.' }}</p>
+              @enderror
             </div>
           </div>
+
+
           <!-- Username -->
           <div class="md:space-y-2.5 space-y-1.5">
             <label for="username" class="input-label">Username</label>
-            <input type="text" id="username" class="default-input" placeholder="E.g. JohnDoe123">
+            <input type="text" name="username" id="username"
+              class="default-input @error('username') !border-red-500 !bg-red-500/5 @enderror"
+              placeholder="E.g. JohnDoe123">
+            @error('username')
+              <p style="color:red;font-size:0.8rem;margin-top:-0.1rem;">{{ $message }}</p>
+            @enderror
           </div>
           <!-- Email -->
           <div class="md:space-y-2.5 space-y-1.5">
             <label for="email" class="input-label">Email Address</label>
-            <input type="email" id="email" class="default-input" placeholder="E.g. john.doe@email.com">
+            <input type="text" name="email" id="email"
+              class="default-input @error('email') !border-red-500 !bg-red-500/5 @enderror"
+              placeholder="E.g. john.doe@email.com">
+            @error('email')
+              <p style="color:red;font-size:0.8rem;margin-top:-0.1rem;">{{ $message }}</p>
+            @enderror
           </div>
           <div class="md:col-span-2 col-span-1 gap-x-5 grid md:grid-cols-2 grid-cols-1">
             <!-- Password -->
             <div class="md:space-y-2.5 space-y-1.5 md:pb-0 pb-5">
               <label for="password" class="input-label">Password</label>
-              <input type="password" id="password" class="default-input" placeholder="Your Password">
+              <input type="password" name="password" id="password"
+                class="default-input @error('password') !border-red-500 !bg-red-500/5 @enderror"
+                placeholder="Your Password">
+              @error('password')
+                <p style="color:red;font-size:0.8rem;margin-top:-0.1rem;">{{ $message }}</p>
+              @enderror
             </div>
             <!-- Confirm Password -->
             <div class="md:space-y-2.5 space-y-1.5">
-              <label for="confirm-password" class="input-label">Confirm Password</label>
-              <input type="password" id="confirm-password" class="default-input" placeholder="Your Password">
+              <label for="password_confirmation" class="input-label">Confirm Password</label>
+              <input type="password" name="password_confirmation" id="password_confirmation"
+                class="default-input  @error('password') !border-red-500 !bg-red-500/5 @enderror"
+                placeholder="Your Password">
             </div>
             <!-- Checkbox -->
             <div class="flex items-center pt-2.5">
@@ -65,8 +91,8 @@
           <!-- Buttons -->
           <div class="md:col-span-2 col-span-1 flex justify-end gap-4">
             <a href="{{ route('login') }}" class="btn btn-secondary">Back</a>
-            <button type="button" class="btn btn-primary">
-              Next
+            <button class="btn btn-primary">
+              Register
             </button>
           </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,28 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'landing.index')->name('home');
 
 // ------ Authentication Pages ------
+
+
 Route::get('/login', function () {
     return view('auth.login');
 })->name('login');
 
-Route::get('/register', function () {
-    return view('auth.register');
-})->name('register');
-// ------ // Authentication Pages ------
+Route::view('/register', 'auth.register')->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+
 
 // ------ Admin Pages ------
 Route::get('/admin', function () {
     return view('admin.index');
 })->name('admin');
-// ------ // Admin Pages ------
+
 
 // ------ Resident Pages ------
-Route::get('/resident', function () {
+Route::get('/dashboard', function () {
     return view('resident.index');
 })->name('resident');
-// ------ // Resident Pages ------
