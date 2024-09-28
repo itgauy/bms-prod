702d45096575061ad6e0170ff2e5ac4d95047623
itgauy <torreslian@duck.com>
2024-09-28 16:32:48 +0800

    Refactor admin dashboard layout and enhance sidebar component functionality

--- resources/views/components/resident/logged-in-sidebar.blade.php
+++ resources/views/components/resident/logged-in-sidebar.blade.php
@@ -8,45 +8,73 @@
 
     <!-- Links -->
     <ul class="flex flex-col items-start gap-1">
-      <a href="{{ route('resident') }}" class="side-nav-link flex w-full items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-stone-700 hover:bg-stone-100 focus:outline-none focus:bg-gray-100 transition duration-300" data-hs-overlay="#main-sidebar">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="shrink-0 size-4">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+      <a href="{{ route('resident') }}"
+        class="side-nav-link flex w-full items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-stone-700 hover:bg-stone-100 focus:outline-none focus:bg-gray-100 transition duration-300"
+        data-hs-overlay="#main-sidebar">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+          class="shrink-0 size-4">
+          <path stroke-linecap="round" stroke-linejoin="round"
+            d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
         </svg>
         User Overview
       </a>
-      <a href="{{ route('home') }}" class="side-nav-link flex w-full items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-stone-700 hover:bg-stone-100 focus:outline-none focus:bg-gray-100 transition duration-300" data-hs-overlay="#main-sidebar">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="shrink-0 size-4">
-          <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+      <a href="{{ route('home') }}"
+        class="side-nav-link flex w-full items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-stone-700 hover:bg-stone-100 focus:outline-none focus:bg-gray-100 transition duration-300"
+        data-hs-overlay="#main-sidebar">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+          stroke="currentColor" class="shrink-0 size-4">
+          <path stroke-linecap="round" stroke-linejoin="round"
+            d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
         </svg>
         Home
       </a>
-      <a href="{{ route('home') }}#announcements" class="side-nav-link flex w-full items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-stone-700 hover:bg-stone-100 focus:outline-none focus:bg-gray-100 transition duration-300" data-hs-overlay="#main-sidebar">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="shrink-0 size-4">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 1 1 0-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 0 1-1.44-4.282m3.102.069a18.03 18.03 0 0 1-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 0 1 8.835 2.535M10.34 6.66a23.847 23.847 0 0 0 8.835-2.535m0 0A23.74 23.74 0 0 0 18.795 3m.38 1.125a23.91 23.91 0 0 1 1.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 0 0 1.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 0 1 0 3.46" />
+      <a href="{{ route('home') }}#announcements"
+        class="side-nav-link flex w-full items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-stone-700 hover:bg-stone-100 focus:outline-none focus:bg-gray-100 transition duration-300"
+        data-hs-overlay="#main-sidebar">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+          stroke="currentColor" class="shrink-0 size-4">
+          <path stroke-linecap="round" stroke-linejoin="round"
+            d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 1 1 0-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 0 1-1.44-4.282m3.102.069a18.03 18.03 0 0 1-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 0 1 8.835 2.535M10.34 6.66a23.847 23.847 0 0 0 8.835-2.535m0 0A23.74 23.74 0 0 0 18.795 3m.38 1.125a23.91 23.91 0 0 1 1.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 0 0 1.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 0 1 0 3.46" />
         </svg>
         Announcements
       </a>
-      <a href="{{ route('home') }}#services" class="side-nav-link flex w-full items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-stone-700 hover:bg-stone-100 focus:outline-none focus:bg-gray-100 transition duration-300" data-hs-overlay="#main-sidebar">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="shrink-0 size-4">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 0 1 9 9v.375M10.125 2.25A3.375 3.375 0 0 1 13.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 0 1 3.375 3.375M9 15l2.25 2.25L15 12" />
+      <a href="{{ route('home') }}#services"
+        class="side-nav-link flex w-full items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-stone-700 hover:bg-stone-100 focus:outline-none focus:bg-gray-100 transition duration-300"
+        data-hs-overlay="#main-sidebar">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+          stroke="currentColor" class="shrink-0 size-4">
+          <path stroke-linecap="round" stroke-linejoin="round"
+            d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 0 1 9 9v.375M10.125 2.25A3.375 3.375 0 0 1 13.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 0 1 3.375 3.375M9 15l2.25 2.25L15 12" />
         </svg>
         Services
       </a>
-      <a href="{{ route('home') }}#officials" class="side-nav-link flex w-full items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-stone-700 hover:bg-stone-100 focus:outline-none focus:bg-gray-100 transition duration-300" data-hs-overlay="#main-sidebar">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="shrink-0 size-4">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
+      <a href="{{ route('home') }}#officials"
+        class="side-nav-link flex w-full items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-stone-700 hover:bg-stone-100 focus:outline-none focus:bg-gray-100 transition duration-300"
+        data-hs-overlay="#main-sidebar">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+          stroke="currentColor" class="shrink-0 size-4">
+          <path stroke-linecap="round" stroke-linejoin="round"
+            d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
         </svg>
         Officials
       </a>
-      <a href="{{ route('home') }}#about-us" class="side-nav-link flex w-full items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-stone-700 hover:bg-stone-100 focus:outline-none focus:bg-gray-100 transition duration-300" data-hs-overlay="#main-sidebar">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="shrink-0 size-4">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+      <a href="{{ route('home') }}#about-us"
+        class="side-nav-link flex w-full items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-stone-700 hover:bg-stone-100 focus:outline-none focus:bg-gray-100 transition duration-300"
+        data-hs-overlay="#main-sidebar">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+          stroke="currentColor" class="shrink-0 size-4">
+          <path stroke-linecap="round" stroke-linejoin="round"
+            d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
         </svg>
         About us
       </a>
-      <a href="{{ route('home') }}#contact-us" class="side-nav-link flex w-full items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-stone-700 hover:bg-stone-100 focus:outline-none focus:bg-gray-100 transition duration-300" data-hs-overlay="#main-sidebar">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="shrink-0 size-4">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+      <a href="{{ route('home') }}#contact-us"
+        class="side-nav-link flex w-full items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-stone-700 hover:bg-stone-100 focus:outline-none focus:bg-gray-100 transition duration-300"
+        data-hs-overlay="#main-sidebar">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+          stroke="currentColor" class="shrink-0 size-4">
+          <path stroke-linecap="round" stroke-linejoin="round"
+            d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
         </svg>
         Contact us
       </a>
@@ -55,20 +83,23 @@
 
   <!-- Logout and User Info -->
   <div class="flex flex-col items-center gap-2.5">
-    <button type="button" class="btn btn-secondary w-full" type="button" aria-haspopup="dialog" aria-expanded="false" aria-controls="logout-modal"
-      data-hs-overlay="#logout-modal">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="shrink-0 size-4">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
+    <button type="button" class="btn btn-secondary w-full" type="button" aria-haspopup="dialog" aria-expanded="false"
+      aria-controls="logout-modal" data-hs-overlay="#logout-modal">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+        class="shrink-0 size-4">
+        <path stroke-linecap="round" stroke-linejoin="round"
+          d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
       </svg>
       Logout
     </button>
     <div class="flex items-start gap-3 w-full border rounded-md p-2">
-      <span class="inline-flex items-center justify-center size-9 text-xs font-semibold rounded-full leading-none border border-stone-300 bg-stone-600 text-white flex-shrink-0">
-        JC
+      <span
+        class="inline-flex items-center justify-center size-9 text-xs font-semibold rounded-full leading-none border border-stone-300 bg-stone-600 text-white flex-shrink-0">
+        {{ strtoupper(substr(auth()->user()->username, 0, 1)) }}
       </span>
       <div>
-        <h4 class="text-stone-700 text-sm">Juan Dela Cruz</h4>
-        <p class="text-stone-600 text-xs">juan.delacruz@email.com</p>
+        <h4 class="text-stone-700 text-sm">{{ auth()->user()->username }}</h4>
+        <p class="text-stone-600 text-xs">{{ auth()->user()->email }}</p>
       </div>
     </div>
   </div>
