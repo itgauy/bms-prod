0f506dee03b77a0f8b24fae9d77bd05ec409a1d8
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-10-03 16:15:54 +0800

    "Updated admin sidebar and layout components: modified button class in sidebar, updated logo, title, and description in layout, and added location and phone number details."

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -145,11 +145,26 @@
     <!-- Logo, Title and description -->
     <div class="space-y-4">
       <a href="{{ route('home') }}" class="inline-flex flex-col gap-3">
-        <span class="block rounded-full size-12 bg-neutral-300 ring-2 ring-neutral-400"></span>
-        <h3 class="text-neutral-200">BNN Chuchu</h3>
+        <span class="block rounded-full size-20 bg-neutral-300 ring-2 ring-neutral-400"></span>
+        <h3 class="text-neutral-200">Barangay Nagkaisang Nayon</h3>
       </a>
-      <p class="w-full max-w-md text-neutral-300">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis
-        accusantium soluta commodi ab, praesentium harum ad id remum harum ad id.</p>
+      <p class="w-full max-w-lg text-neutral-300 text-sm">Nestled in District V, Area IX of Quezon City, lies Barangay Nagkaisang Nayon, a vibrant community. The name itself, "Nagkaisang Nayon," translates to "United Villages," hinting at the strong sense of community spirit that thrives here.</p>
+      <div class="space-y-1">
+        <!-- Location -->
+        <span class="flex text-sm items-start gap-2 text-neutral-300">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4">
+            <path fill-rule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clip-rule="evenodd" />
+          </svg>
+          673 Quirino Highway, San Bartolome, Novaliches, Quezon City
+        </span>
+        <!-- Phone.Contact Number -->
+        <span class="flex text-sm items-start gap-2 text-neutral-300">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4">
+            <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clip-rule="evenodd" />
+          </svg>
+          (02) 8806-3049
+        </span>
+      </div>
     </div>
     <!-- Links if any -->
     <div class="flex gap-3">
