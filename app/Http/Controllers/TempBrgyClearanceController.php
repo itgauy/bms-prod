40cc1e171cc06adc5c1db99a42c68341e775c624
itgauy <torreslian@duck.com>
2024-11-15 00:06:17 +0800

    IN PROGRESS: Temporary Brgy Clearance Controller

--- app/Http/Controllers/TempBrgyClearanceController.php
+++ app/Http/Controllers/TempBrgyClearanceController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\BrgyClearance;
 
 class TempBrgyClearanceController extends Controller
 {
-    //
+    public function brgyClearance(Request $request) {
+        $brgyFields = $request->validate([
+            'category' => ['required'],
+            'name' => ['required', 'max:255'],
+            'position' => ['required', 'max:255'],
+            'business_type' => ['required', 'max:255'],
+            'business_name' => ['required', 'max:255'],
+            'business_address' => ['required', 'max:255'],
+            'date_claimed' => ['required', 'date'],
+            'contact_person' => ['required', 'max:255'],
+            'contact_position' => ['required', 'max:255'],
+            'contact_number' => ['required', 'max:20'],
+        ]);
+
+        // Register brgy clearance
+        BrgyClearance::create($brgyFields);
+
+        // Redirect
+        return redirect()->route('brgy-clearance');
+    }
 }
